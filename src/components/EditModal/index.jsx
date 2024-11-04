1d5ee4a77c039f0c4e7c19ab2679d52826e379b0
Vinicius Caua <vinipg1@hotmail.com>
2024-11-04 10:33:34 -0300

    Configurando data no card para formato dd/mm/aaaa

--- src/components/EditModal/index.jsx
+++ src/components/EditModal/index.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import api from "../../api";
 import Modal from "../Modal";
+import { formatarData } from "../../utils";
 
 function EditModal({ isOpen, task, onCancel }) {
   const [formValues, setFormValues] = useState({
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,8 +1,11 @@
 // Função para formatar a data para "dd/mm/aaaa"
 export function formatarData(data) {
-  const ano = data[0];
-  const mes = data[1];
-  const dia = data[2];
+  if (!data) return ""; // Retorna vazio se não houver data
 
-  return data ? `${dia}/${mes}/${ano}` : "";
+  const date = new Date(data); // Cria um objeto Date a partir da string
+  const dia = String(date.getDate()).padStart(2, "0"); // Obtém o dia com zero à esquerda
+  const mes = String(date.getMonth() + 1).padStart(2, "0"); // Obtém o mês (0-11) e adiciona 1
+  const ano = date.getFullYear(); // Obtém o ano
+
+  return `${dia}/${mes}/${ano}`; // Retorna a data formatada
 }
