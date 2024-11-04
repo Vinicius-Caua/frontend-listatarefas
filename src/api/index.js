d8dfea87a229b7f0c242cd95affe68b3750d39b9
Vinicius Caua <vinipg1@hotmail.com>
2024-11-04 10:37:18 -0300

    Rota adequada configurada

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const apiUrl = "https://backend-listatarefas-production.up.railway.app";
-const apiUrl = "http://localhost:8080";
+const apiUrl = "https://backend-listatarefas-production.up.railway.app";
+// const apiUrl = "http://localhost:8080";
 
 const api = axios.create({
   baseURL: apiUrl,
