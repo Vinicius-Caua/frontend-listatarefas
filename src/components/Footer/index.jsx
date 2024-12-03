e3a47bbf7ee83110a948cb593c30ccb222cbaf49
Vinicius Caua <vinipg1@hotmail.com>
2024-12-03 16:16:09 -0300

    alterando footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,10 +1,10 @@
-import fatto from "../../assets/fatto.png";
+import cachorro from "../../assets/cachorro.png";
 
 function Footer() {
   return (
     <div className="bg-dark flex w-full h-24 items-center gap-4 justify-center">
-      <img src={fatto} height={30} width={30} />
-      <h1 className="text-white">© FATTO, todos os direitos reservados.</h1>
+      <img src={cachorro} height={30} width={30} />
+      <h1 className="text-white">Direitos reservados.</h1>
     </div>
   );
 }
